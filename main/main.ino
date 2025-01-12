2bd1e07fc9be3efeaeb2559ed84d98f9e42a5bb8
AliceEnjoyer <Ilin06Serhii@gmail.com>
2025-01-12 11:01:38 +0200

    added new funcs, structs, new config parts of code linked with SD, RTC, LCD, SMS

--- main/main.ino
+++ main/main.ino
@@ -1,92 +1,189 @@
 #include <math.h>
 #include <DoubleLinkedList.h>
+#include <LiquidCrystal_SoftI2C.h>
+#include <SoftwareWire.h>
+#include <SPI.h>
+#include <SD.h>
 
-#define DEBUG 1
 #define CHECK Serial.println("✓");
 #define DEFAULT_TIMEOUT 2000
 #define RED_NAME "Obj1" // RED - Refrigeration Exporter of Data
 
 const int thermistor_output1 = A0;
-const int reset_pin = 6;
+const int reset_pin = 11; // with multiplexer is A3
+const int chipSelect_pin = 10;
+const int SDA_pin = 6;
+const int SCL_pin = 7;
+
+const uint8_t RTC_ADDRESS = 0x68;
+const uint8_t LCD_ADDRESS = 0x27;
+
+SoftwareWire myWire(SDA_pin, SCL_pin);
+LiquidCrystal_I2C lcd(LCD_ADDRESS,16,2, &myWire);
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+File myFile;
+
+struct MyDateTime {
+  uint16_t year;
+  uint8_t month;
+  uint8_t day;
+  uint8_t hour;
+  uint8_t minute;
+  uint8_t second;
+
+  MyDateTime(uint16_t y, uint8_t mo, uint8_t d, uint8_t h, uint8_t mi, uint8_t s)
+      : year(y), month(mo), day(d), hour(h), minute(mi), second(s) {}
+};
 
 void arduinoReset();
 void sendData(const String& command, const int timeout = DEFAULT_TIMEOUT, void(*funcIfNotOk)() = arduinoReset);
 String getData(const String& command, const int timeout = DEFAULT_TIMEOUT);
+
 void sendTempreature(const String& targetURL, float tempreature, String termistorpPath);
 float TempreatureFromAdc(const int16_t& thermistor_adc_val);
+
 bool findOk(const String& txt);
 void plug() {}
 
+uint8_t bcdToDec(uint8_t val);
+uint8_t decToBcd(uint8_t val);
+void setRTC(const MyDateTime &dt);
+MyDateTime getRTC();
+
+void printLCD(const String& txt, const String& txt2 = "");
+
+bool isURLOK = 0;
+bool isRTCOK = 0;
 
 
 void setup() {
+#ifdef DEBUG
   Serial.begin(9600);
-  Serial1.begin(9600);
   while(!Serial);
-  while(!Serial1);
+#endif
+  myWire.begin();
+  lcd.begin();                      
+  lcd.backlight();
+  printLCD("LCD and wire OK");
 
+  Serial1.begin(9600);
+  while(!Serial1);
+  printLCD("Serial1 OK");  
+  
   //Power on the SIM800C
   pinMode(9,OUTPUT);
   digitalWrite(9,HIGH);
   delay(3000);
   digitalWrite(9,LOW);
   delay(1000);
-      
+  printLCD("SIM800C OK");
 
-  Serial.println(" -- Program start -- ");  
-  Serial.println("Serial initialization... ✓");  
-  Serial.print("Serial1 initialization... ");  
   while(!Serial1.availableForWrite());
-  CHECK
-  Serial.println("AT commands initialization... ");    
+  printLCD("Serial1 is write", "available");
+
+  printLCD("AT commands", "initialization.. ");    
   sendData("AT");
-  Serial.println("\n");
+  printLCD("AT OK");
 
-  Serial.println("Sim-card setup... ");    
+  printLCD("Sim-card setup..");    
   sendData("AT+CSQ"); // checking signal level
+  printLCD("AT+CSQ OK");
   sendData("AT+CREG?"); // checking web registration 
+  printLCD("AT+CREG? OK");
   sendData("AT+CSTT=\"internet.kyivstar.net\",\"\",\"\""); // installing APN
+  printLCD("Kyivstar OK");
   sendData("AT+CIICR"); // Raising a wireless connection
+  printLCD("AT+CIICR OK");
   sendData("AT+SAPBR=3,1,\"Contype\",\"GPRS\""); // Setting media profile parameters
+  printLCD("GPRS OK");
   sendData("AT+SAPBR=3,1,\"APN\",\"internet.kyivstar.net\""); // installing APN
+  printLCD("APN OK");
   sendData("AT+SAPBR=1,1"); // Opening the carrier profile
+  printLCD("SAPBR1.1 OK");
   sendData("AT+SAPBR=2,1"); // Checking the carrier profile status
-  Serial.println("\n");
+  printLCD("SAPBR2.1 OK");
+  sendData("AT+CMGF=1"); // Set the SMS in text mode
+  printLCD("SMS OK");
 
-  // Serial.println("Enabling server settings... ");    
-  // Serial1.println("AT+CIPMUX=1");            // Включаем мультиканальный режим
-  // delay(1000);
-  // Serial1.println("AT+CIPSERVER=1,80");      // Открываем сервер на порту 80
-  // delay(1000);
+  printLCD("Initializing", "SD card..");
+  if (!card.init(SPI_HALF_SPEED, chipSelect_pin)) {
+    printLCD("SD module BAD", "Reseting...");
+    delay(5000);
+    arduinoReset();
+  } else {
+    printLCD("SD module OK");
+  }
 
-
-  Serial.println("HTTP initialization... ");    
+  printLCD("HTTP", "initialization..");    
   sendData("AT+HTTPINIT");
-  Serial.println("\n");
+  printLCD("AT+HTTPINIT OK");
 
-  Serial.println("HTTP parameters setting... ");  
+  printLCD("HTTP parameters", "setting...");  
   sendData("AT+HTTPPARA=\"CID\", 1");
-  //sendData("AT+HTTPPARA=\"URL\", \"http://92.43.81.152:1488/data\"");
+  printLCD("CID OK");
+  // sendData("AT+HTTPPARA=\"URL\", \"http://92.43.81.152:1488/data\"");
+  // printLCD("URL OK");
   sendData("AT+HTTPPARA=\"CONTENT\", \"application/json\"");
+  printLCD("CONTENT OK");
   sendData("AT+HTTPPARA?");
+  printLCD("AT+HTTPPARA? OK");
 
-  Serial.println("\n");
-  Serial.println(" -- Data transfer start -- ");  
+  if(SD.exists("url.txt")) {
+    myFile = SD.open("url.txt", FILE_READ);
+    String url;
+    while (myFile.available()) {
+      url += myFile.read();
+    }
+    sendData("AT+HTTPPARA=\"URL\", \""+url+"\"");
+    printLCD("URL OK");
+    isURLOK = 1;
+    myFile.close();
+  }
+
+  if(getRTC().year < 2025){
+    printLCD("RTC BAD");
+    if(isURLOK){
+      // TODO: request on server for time init
+      setRTC(MyDateTime(2025, 1, 11, 13, 45, 33));
+      printLCD("RTC OK");
+      isRTCOK = 1;
+    }
+  } else{
+    printLCD("RTC OK");
+    isRTCOK = 1;
+  }
+  
+  if(isURLOK){
+    printLCD("Data transfering", "started");  
+  } else {
+    printLCD("Waiting URL via","SMS (user app)");  
+  }
 
   pinMode(thermistor_output1, INPUT);  
 }
   
 void loop() {
   if (Serial1.available()) {
-        String message = Serial1.readString();
-        if (message.indexOf("+CMT:") != -1) {
-            // Извлечение текста SMS
-            int index = message.indexOf("\r\n") + 2;
-            String command = message.substring(index);
-            Serial.println(message)
-            Serial.println(command)
-        }
+    String message = Serial1.readString();
+
+    // Проверка на наличие заголовка SMS
+    if (message.indexOf("+CMT:") != -1) {
+        // Извлечение текста SMS
+        int index = message.indexOf("\r\n") + 2; // Найти начало текста SMS
+        String command = message.substring(index); // Извлечь текст сообщения
+        // TODO: сделать проверку с помощью регуляторных выражений
+        // на наличие адреса на сервер. В случае положительной проверки
+        // настроить url для SIM800C, записать этот url в url.txt,
+        // поставить значение isURLOK = 1, а также настроить RTC
+        // после чего поставить занчение isRTCOK = 1
     }
+  }
+  if(isURLOK){
+    // сделать трансферинг данных с датчиков на сервер/SD-карту 
+    // с (возможно "красивыми") часовыми метками 
+  }
 }
 
 void sendData(const String& command, const int timeout = DEFAULT_TIMEOUT, void(*funcIfNotOk)() = arduinoReset) { //Send command function
@@ -98,8 +195,20 @@ void sendData(const String& command, const int timeout = DEFAULT_TIMEOUT, void(*
     while(Serial1.available()){       
       response += (char)Serial1.read(); 
     }  
-  }    
-  if(DEBUG){
+  }
+  if(!findOk(response)) {
+      lcd.clear();
+      lcd.print("BAD");
+      if(funcIfNotOk == arduinoReset){
+        lcd.setCursor(0, 1);
+        lcd.print("Reseting...");
+        lcd.setCursor(0, 0);
+        delay(5000);  
+      }
+      funcIfNotOk();
+  }
+
+  #ifdef DEBUG
     Serial.println();
     Serial.print(response);
     CHECK
@@ -110,7 +219,7 @@ void sendData(const String& command, const int timeout = DEFAULT_TIMEOUT, void(*
     }
 
     Serial.println();
-  }  
+  #endif
 }
 
 String getData(const String& command, const int timeout = DEFAULT_TIMEOUT) {
@@ -122,12 +231,12 @@ String getData(const String& command, const int timeout = DEFAULT_TIMEOUT) {
         response += (char)Serial1.read(); 
       }  
     }    
-    if(DEBUG){
+    #ifdef DEBUG
       Serial.println();
       Serial.print(response);
       CHECK
       Serial.println();
-    }
+    #endif
 
     return response;
 }
@@ -177,6 +286,59 @@ void arduinoReset() {
   digitalWrite(reset_pin, HIGH);  
 }
 
+// Преобразование из двоично-десятичного формата в десятичный
+uint8_t bcdToDec(uint8_t val) {
+  return (val / 16 * 10) + (val % 16);
+}
+
+// Преобразование из десятичного формата в двоично-десятичный
+uint8_t decToBcd(uint8_t val) {
+  return (val / 10 * 16) + (val % 10);
+}
+
+// Функция для записи времени в RTC
+void setRTC(const MyDateTime &dt) {
+  myWire.beginTransmission(RTC_ADDRESS);
+  myWire.write(0x00); // Указываем адрес регистра секунд
+  myWire.write(decToBcd(dt.second));
+  myWire.write(decToBcd(dt.minute));
+  myWire.write(decToBcd(dt.hour));
+  myWire.write(decToBcd(0)); // День недели (не используется)
+  myWire.write(decToBcd(dt.day));
+  myWire.write(decToBcd(dt.month));
+  myWire.write(decToBcd(dt.year - 2000));
+  myWire.endTransmission();
+}
+
+// Функция для чтения времени из RTC
+MyDateTime getRTC() {
+  myWire.beginTransmission(RTC_ADDRESS);
+  myWire.write(0x00); // Указываем адрес регистра секунд
+  myWire.endTransmission();
+
+  myWire.requestFrom(RTC_ADDRESS, SCL_pin);
+  uint8_t second = bcdToDec(myWire.read() & 0x7F);
+  uint8_t minute = bcdToDec(myWire.read());
+  uint8_t hour = bcdToDec(myWire.read());
+  myWire.read(); // Пропускаем день недели
+  uint8_t day = bcdToDec(myWire.read());
+  uint8_t month = bcdToDec(myWire.read());
+  uint16_t year = 2000 + bcdToDec(myWire.read());
+
+  return MyDateTime(year, month, day, hour, minute, second);
+}
+
+void printLCD(const String& txt, const String& txt2 = "") {
+  lcd.clear();
+  lcd.print(txt);
+  if(txt != "") {
+    lcd.setCursor(0, 1);
+    lcd.print(txt2);
+  }
+  lcd.setCursor(0, 0);
+}
+
+
 //const int DanfossAK32R_Pin = A1;
 //typedef int16_t ia; // adc in integer
 //ia U0, U100, P100;
@@ -185,3 +347,10 @@ void arduinoReset() {
 // }
 //  U0 = 162, U100 = 1012, P100 = 14;  // preassure param. that depends on special sensors.
   //ia DanfossAK32R_Output = analogRead(DanfossAK32R_Pin);
+
+
+  // Serial.println("Enabling server settings... ");    
+  // Serial1.println("AT+CIPMUX=1");            // Включаем мультиканальный режим
+  // delay(1000);
+  // Serial1.println("AT+CIPSERVER=1,80");      // Открываем сервер на порту 80
+  // delay(1000);
